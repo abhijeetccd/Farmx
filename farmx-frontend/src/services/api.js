769d590059012cdfe3b8c23d851ce355a8b16f4e
Sanket Gaikwad <sanketlike63@gmail.com>
2025-07-10 15:30:33 +0530

    added or option for locola system

--- farmx-frontend/src/services/api.js
+++ farmx-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "http://192.168.93.242:5000/api";
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
